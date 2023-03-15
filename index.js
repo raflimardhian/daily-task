681fc578b64420c7c774322ee95eb96b08fb0afb
raflimardhian <101852642+raflimardhian@users.noreply.github.com>
2023-03-15 19:40:17 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,169 +1,364 @@
-number = 1
-
-const person = [
-    {
-      "_id": "640b3dc084a37c1d185a47f8",
-      "isActive": false,
-      "age": 38,
-      "eyeColor": "blue",
-      "name": "Lamb Wiley",
-      "gender": "male",
-      "favoriteFruit": "apple"
-    },
-    {
-      "_id": "640b3dc0a933c5d639fda2ca",
-      "isActive": false,
-      "age": 25,
-      "eyeColor": "green",
-      "name": "Jeanne Benjamin",
-      "gender": "female",
-      "favoriteFruit": "strawberry"
-    },
-    {
-      "_id": "640b3dc0c6efe784e2b8c5dd",
-      "isActive": true,
-      "age": 20,
-      "eyeColor": "brown",
-      "name": "Arline Compton",
-      "gender": "female",
-      "favoriteFruit": "apple"
-    },
-    {
-      "_id": "640b3dc01c265c88207888c5",
-      "isActive": true,
-      "age": 35,
-      "eyeColor": "blue",
-      "name": "Francis Hancock",
-      "gender": "female",
-      "favoriteFruit": "strawberry"
-    },
-    {
-      "_id": "640b3dc01bf7307e2f09564f",
-      "isActive": false,
-      "age": 27,
-      "eyeColor": "blue",
-      "name": "Barbara Gardner",
-      "gender": "female",
-      "favoriteFruit": "banana"
-    },
-    {
-      "_id": "640b3dc07f1b275c3b310a69",
-      "isActive": false,
-      "age": 23,
-      "eyeColor": "brown",
-      "name": "Robin Mcdaniel",
-      "gender": "female",
-      "favoriteFruit": "strawberry"
-    },
-    {
-      "_id": "640b3dc0e2ee43739bf9134e",
-      "isActive": true,
-      "age": 33,
-      "eyeColor": "brown",
-      "name": "Lucy Cantu",
-      "gender": "female",
-      "favoriteFruit": "apple"
-    },
-    {
-      "_id": "640b3dc0c7e2c148e93b768c",
-      "isActive": false,
-      "age": 31,
-      "eyeColor": "brown",
-      "name": "Le Oneil",
-      "gender": "male",
-      "favoriteFruit": "strawberry"
-    },
-    {
-      "_id": "640b3dc0672e98c64d84be7e",
-      "isActive": true,
-      "age": 38,
-      "eyeColor": "brown",
-      "name": "Battle Solis",
-      "gender": "male",
-      "favoriteFruit": "strawberry"
-    },
-    {
-      "_id": "640b3dc052e660068592b6ed",
-      "isActive": true,
-      "age": 21,
-      "eyeColor": "green",
-      "name": "Ward Irwin",
-      "gender": "male",
-      "favoriteFruit": "banana"
-    }
+let data = [
+  {
+    "_id": "64119b7c974c2de5c18cec62",
+    "age": 24,
+    "eyeColor": "blue",
+    "name": "Ladonna Holcomb",
+    "address": "Bali",
+    "registered": "2021-04-16T01:26:00 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "banana",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Theresia",
+        "isActive": false,
+        "favoriteFruit": "apple",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "baseball"
+          },
+          {
+            "id": 2,
+            "hobby": "baseball"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64119b7c29ca71225f5bd07a",
+    "age": 39,
+    "eyeColor": "brown",
+    "name": "Lynch Salazar",
+    "address": "Bali",
+    "registered": "2021-06-03T07:18:09 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Farid",
+        "isActive": true,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "football"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64119b7c670e0827e667ebe6",
+    "age": 23,
+    "eyeColor": "green",
+    "name": "Cantu Drake",
+    "address": "Bali",
+    "registered": "2020-12-23T08:30:58 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 2,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64119b7c1a043539cb9679c2",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": "Langley Hodges",
+    "address": "Bandung",
+    "registered": "2022-05-25T04:19:31 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Farid",
+        "isActive": true,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 2,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Theresia",
+        "isActive": true,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 2,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64119b7c35b03ce85926c7a1",
+    "age": 27,
+    "eyeColor": "green",
+    "name": "Annette Bolton",
+    "address": "Bandung",
+    "registered": "2020-03-18T12:47:47 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "banana",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 1,
+            "hobby": "football"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Farid",
+        "isActive": true,
+        "favoriteFruit": "apple",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Theresia",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  }
 ]
 
+let person = []
 
-// -----nomor1
-for (let i=0;i<person.length; i++) {
-    if (person[i].favoriteFruit === "banana" && person[i].eyeColor === "green") {
-      console.log("helo")
-        console.log(person[i])
-    } else {}
+
+// const result = data.filter (function(persons) {
+//   return persons.age < 30
+// })
+// console.log(result)
+
+// -----nomor 1
+
+
+
+// ------nomor 2
+// let person = []
+
+// const result = data.filter (function (persons) {
+//   return persons.address === "Bali"
+// })
+// console.log(result)
+
+// -----nomor 3
+// let result = data.map (function (person) {
+//   return person.friends.filter (function (friend){
+//     return friend.hobby.filter (function (persons){
+//       return persons.hobby === "football"
+//     })
+//   })
+// })
+// console.log(result)
+
+// ----- nomor 4
+// let result = data.map (function (person) {
+//   return person.friends.filter (function (friend){
+//     return friend.hobby.filter (function (persons){
+//       return persons.id === 2
+//     })
+//   })
+// })
+// console.log(result)
+
+// -----nomor 5
+// let result = data.filter (function(person){
+//   return person.friends.filter (function (persons){
+//     return persons.gender === "male" && persons.hobby === "basketball"
+//   })
+// })
+
+
+// -----nomor 6
+for(let i=0; i<data.length; i++){
+  for(let j=0; j<data[i].friends.length;){
+    if (data[i].friends[j].isActive === true && data[i.friends[j].gender === "female" && data[i].friends[j].favoriteFruit === "strawberry"){
+      console.log(data[i].friends[j])
+    }
+  }
 }
-
-
-// -----nomor2
-// let arr = []
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].age > 30) {
-//       arr.push(person[i])
-//   }
-// }
-// console.log(arr.length)
-
-// -----nomor3
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].isActive === true && person[i].gender ==="female") {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// -----nomor4
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].age < 25 || person[i].age > 30 && person[i].eyeColor === "blue") {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// ----nomor5
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].name.match("w")) {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// -----nomor6
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].age > 25 && person[i].isActive == false && person[i].gender == "male") {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// -----nomor7
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].age < 30 || person[i].eyeColor === "brown" && person[i].favoriteFruit === "banana") {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// -----nomor8
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].gender === "male") {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// -----nomor9
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].eyeColor === "blue") {
-//       console.log(person[i])
-//   } else {}
-// }
-
-// -----nomor10
-// for (let i=0;i<person.length; i++) {
-//   if (person[i].age > 30) {
-//       console.log(person[i])
-//   } else {}
-// }
